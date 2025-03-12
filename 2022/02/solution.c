d2d1336c071fcc1d5b0da6df8b2785ea43a12428
Liam Mitchell <git@liam.geek.nz>
2025-03-12 08:41:21 +0100

    Switch to Chromium style

--- 2022/02/solution.c
+++ 2022/02/solution.c
@@ -2,35 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-void part1(const char *name)
-{
-  FILE *file = fopen(name, "r");
+void part1(const char* name) {
+  FILE* file = fopen(name, "r");
   time_t start = time(NULL);
 
   int result = 0;
   char theirHand;
   char ourHand;
-  while (fscanf(file, "%c %c ", &theirHand, &ourHand) > 0)
-  {
+  while (fscanf(file, "%c %c ", &theirHand, &ourHand) > 0) {
     theirHand -= 'A';
     ourHand -= 'X';
     int score = (((ourHand - theirHand) + 4) % 3) * 3;
     result += score + ourHand + 1;
   }
 
-  printf("Part 1 %s %d %.0lfms\n", name, result, difftime(time(NULL), start) / 1000);
+  printf("Part 1 %s %d %.0lfms\n", name, result,
+         difftime(time(NULL), start) / 1000);
 }
 
-void part2(const char *name)
-{
-  FILE *file = fopen(name, "r");
+void part2(const char* name) {
+  FILE* file = fopen(name, "r");
   time_t start = time(NULL);
 
   int result = 0;
   char theirHand;
   char expectedResult;
-  while (fscanf(file, "%c %c ", &theirHand, &expectedResult) > 0)
-  {
+  while (fscanf(file, "%c %c ", &theirHand, &expectedResult) > 0) {
     theirHand -= 'A';
     expectedResult -= 'X';
     int ourHand = (theirHand + expectedResult + 2) % 3;
@@ -38,11 +35,11 @@ void part2(const char *name)
     result += score + ourHand + 1;
   }
 
-  printf("Part 2 %s %d %.0lfms\n", name, result, difftime(time(NULL), start) / 1000);
+  printf("Part 2 %s %d %.0lfms\n", name, result,
+         difftime(time(NULL), start) / 1000);
 }
 
-int main(void)
-{
+int main(void) {
   part1("2022/02/example.txt");
   part1("2022/02/input.txt");
   part2("2022/02/example.txt");
