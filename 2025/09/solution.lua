85d88d5f4aa36b755c26b2252d58a0f9d2c42a1b
Liam Mitchell <git@liam.geek.nz>
2025-12-09 19:50:20 +0100

    Faster

--- 2025/09/solution.lua
+++ 2025/09/solution.lua
@@ -22,6 +22,27 @@ local function part2(name)
 		local x, y = string.match(line, "(%d+),(%d+)")
 		table.insert(tiles, { tonumber(x), tonumber(y) })
 	end
+	-- prepare tables of sorted horizontal and vertical lines
+	local horizontal = {}
+	local vertical = {}
+	for i, tile in ipairs(tiles) do
+		local nexttile = tiles[i % #tiles + 1]
+		local minx = math.min(tile[1], nexttile[1])
+		local maxx = math.max(tile[1], nexttile[1])
+		local miny = math.min(tile[2], nexttile[2])
+		local maxy = math.max(tile[2], nexttile[2])
+		if minx == maxx then
+			table.insert(vertical, { minx, miny, maxx, maxy })
+		else
+			table.insert(horizontal, { minx, miny, maxx, maxy })
+		end
+	end
+	table.sort(vertical, function(a, b)
+		return a[1] < b[1]
+	end)
+	table.sort(horizontal, function(a, b)
+		return a[2] < b[2]
+	end)
 	local count = 0
 	for i = 1, #tiles - 1 do
 		local ix = tiles[i][1]
@@ -35,34 +56,24 @@ local function part2(name)
 			local maxy = math.max(iy, jy)
 			local area = (1 + maxx - minx) * (1 + maxy - miny)
 			if area > count then
-				local whole = true
 				-- Check for lines through area.
-				for k, tile in ipairs(tiles) do
-					local kx = tile[1]
-					local ky = tile[2]
-					if kx > minx and kx < maxx then
-						local nexttile = tiles[k % #tiles + 1]
-						local kminy = math.min(tile[2], nexttile[2])
-						local kmaxy = math.max(tile[2], nexttile[2])
-						if not (kminy >= maxy or kmaxy <= miny) then
-							whole = false
-							break
-						end
-					end
-					if ky > miny and ky < maxy then
-						local nexttile = tiles[k % #tiles + 1]
-						local kminx = math.min(tile[1], nexttile[1])
-						local kmaxx = math.max(tile[1], nexttile[1])
-						if not (kminx >= maxx or kmaxx <= minx) then
-							whole = false
-							break
-						end
+				for _, line in ipairs(horizontal) do
+					if line[2] > miny and line[2] < maxy and not (line[1] >= maxx or line[3] <= minx) then
+						goto continue
+					elseif line[2] > maxy then
+						break
 					end
 				end
-				if whole == true then
-					count = area
+				for _, line in ipairs(vertical) do
+					if line[1] > minx and line[1] < maxx and not (line[2] >= maxy or line[4] <= miny) then
+						goto continue
+					elseif line[1] > maxx then
+						break
+					end
 				end
+				count = area
 			end
+			::continue::
 		end
 	end
 	print("Part 2 " .. name .. " " .. count .. " in " .. os.clock() - start .. " seconds")
